0ce9e547f1391b440d014dcb26810585dabcb5ef
Gustavo Turpo <gturpot@unsa.edu.pe>
2020-08-10 15:24:03 -0500

    Pagina de inicio con bootstrap

--- tienda/views.py
+++ tienda/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from productos.models import Producto
 
 # Create your views here.
+def homeView(request):
+  data = {
+    "name": "Gustavo",
+    "productos": Producto.objects.all()
+  }
+  return render(request,"index.html", data)
--- tiendaVirtual/urls.py
+++ tiendaVirtual/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from tienda.views import homeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('productos/', include('productos.urls')),
+    path('', homeView)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
